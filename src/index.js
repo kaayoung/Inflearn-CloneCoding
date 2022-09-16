f56615fbe52535a0ec55cda9c937f0f5e8888a78
kaayoung <20190856@sungshin.ac.kr>
2022-09-16 17:55:23 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css_file/reset.css';
+import './css_file/variable.css';
+import './index.css'
+// import './css_file/header.css';
+// import App from './App';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Main_Page from './components/Main_Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Header></Header>
+    <Nav></Nav>
+    <Main_Page></Main_Page>
+    <Footer></Footer>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
